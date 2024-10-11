75ce24624c6cb845f9c325dc57eaf28189978689
adriapedralbes <a23adrestmar@inspedralbes.cat>
2024-10-11 09:31:48 +0200

    Update config.php

--- back/config.php
+++ back/config.php
@@ -1,6 +1,6 @@
 <?php
     $db_server = 'localhost:3306';
-    $db_username = 'a23adrestmar_adrianquiz ';
+    $db_username = 'a23adrestmar_adrianquiz';
     $db_password = 'Adria123';
     $db_name = 'a23adrestmar_adria';
 ?>
